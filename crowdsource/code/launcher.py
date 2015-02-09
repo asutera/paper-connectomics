cbed92f68c2427bf0818ddec4c5fe21f36f3db99
Arnaud Joly <arnaud.v.joly@gmail.com>
2015-02-09 14:05:34 +0100

    Try to fix shape issue

--- crowdsource/code/launcher.py
+++ crowdsource/code/launcher.py
@@ -101,7 +101,7 @@ def compute_scores(f_ground_truth, f_prediction, parameters):
         # make a mask
         alive = np.ones((y_true.shape[0],), dtype=bool)
         alive[kill - 1] = False  # we need to make -1 since it's matlab indexing
-        y_true = y_true[alive, alive]
+        y_true = y_true[alive][alive]
         print(y_true.sum())
 
 
