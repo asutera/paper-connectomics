b8bb850ca3e36e5d759695ed8097e59375879621
Arnaud Joly <arnaud.v.joly@gmail.com>
2015-11-02 08:42:59 +0100

    Be sure to avoid duplicate

--- crowdsource/code/launcher.py
+++ crowdsource/code/launcher.py
@@ -30,9 +30,8 @@ for directory in [os.path.join(WORKING_DIR, "datasets"),
             if path.startswith("fluorescence_"):
                 ALL_FLUORESCENCE.append(os.path.join(directory, path))
 
-ALL_NETWORKS = [os.path.basename(os.path.splitext(x)[0]).split("_", 1)[1]
-                for x in ALL_FLUORESCENCE]
-
+ALL_NETWORKS = set(os.path.basename(os.path.splitext(x)[0]).split("_", 1)[1]
+                   for x in ALL_FLUORESCENCE)
 
 OUTPUT_DIR = os.path.join(WORKING_DIR, "submission")
 
